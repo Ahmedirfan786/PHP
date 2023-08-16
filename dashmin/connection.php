395f6ff338f872f9a757ede7149dd398f0ce167c
Ahmedirfan786 <129369417+Ahmedirfan786@users.noreply.github.com>
2023-08-16 12:40:40 -0700

    Delete dashmin directory

--- dashmin/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$server = "mysql:host=localhost;dbname=dashmin_ahmed";
-$user="root";
-$password="";
-$pdo=new PDO($server,$user,$password);
-?>
--- dashmin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-echo"
-<script>location.assign('signin.php')</script>
-";
-?>
